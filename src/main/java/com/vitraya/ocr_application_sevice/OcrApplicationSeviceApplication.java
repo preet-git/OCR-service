8fa331a514713c0d3a453b1e2e6725c3fe0f27dc
Preet Ahlawat <preetahlawat@Preets-MacBook-Air-2.local>
2024-07-27 15:51:10 +0530

    First commit

---
+++ src/main/java/com/vitraya/ocr_application_sevice/OcrApplicationSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.vitraya.ocr_application_sevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrApplicationSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrApplicationSeviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitraya/ocr_application_sevice/OcrApplicationSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitraya.ocr_application_sevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrApplicationSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
